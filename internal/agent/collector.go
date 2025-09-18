66fdcc71bd6938ea6cb4db5810a8b9270736d274
bq2cd <231322472+bq2cd@users.noreply.github.com>
2025-09-18 15:59:07 +0300

    agent: collect runtime metrics from runtime.MemStats
    It was chosed to use reflection to retrieve actual values from the
    'runtime.MemStat' struct to avoid duplicating metric names in multiple
    places.

--- internal/agent/collector.go
+++ internal/agent/collector.go
@@ -1,6 +1,12 @@
 package agent
 
-import "github.com/bq2cd/yp-go-metrics/internal/model"
+import (
+	"math/rand/v2"
+	"reflect"
+	"runtime"
+
+	"github.com/bq2cd/yp-go-metrics/internal/model"
+)
 
 var (
 	defaultRuntimeMetrics = map[string]model.MetricType{
@@ -54,5 +60,60 @@ func NewDefaultCollector() *defaultCollector {
 
 // Collect returns collected metrics.
 func (c *defaultCollector) Collect() ([]model.Metric, error) {
-	return nil, nil
+	c.pollCounter++
+	randomValue := rand.Float64()
+
+	metrics := make([]model.Metric, 0, len(defaultRuntimeMetrics)+len(defaultExtraMetrics))
+
+	metrics = append(metrics, model.NewCounterMetric("PollCount", int64(c.pollCounter)))
+	metrics = append(metrics, model.NewGaugeMetric("RandomValue", randomValue))
+
+	var memStats runtime.MemStats
+	runtime.ReadMemStats(&memStats)
+
+	stats := reflect.ValueOf(memStats)
+
+	castToInt64 := func(v reflect.Value) (int64, bool) {
+		if v.CanInt() {
+			return v.Int(), true
+		}
+		if v.CanUint() {
+			return int64(v.Uint()), true
+		}
+		return 0, false
+	}
+
+	castToFloat64 := func(v reflect.Value) (float64, bool) {
+		if value, ok := castToInt64(v); ok {
+			return float64(value), true
+		}
+		if v.CanFloat() {
+			return v.Float(), true
+		}
+		return 0, false
+	}
+
+	for mID, mType := range defaultRuntimeMetrics {
+		value := stats.FieldByName(mID)
+		var metric model.Metric
+		switch mType {
+		case model.MetricTypeCounter:
+			mValue, ok := castToInt64(value)
+			if !ok {
+				continue
+			}
+			metric = model.NewCounterMetric(mID, mValue)
+		case model.MetricTypeGauge:
+			mValue, ok := castToFloat64(value)
+			if !ok {
+				continue
+			}
+			metric = model.NewGaugeMetric(mID, mValue)
+		default:
+			continue
+		}
+		metrics = append(metrics, metric)
+	}
+
+	return metrics, nil
 }
