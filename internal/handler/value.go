777ba2610398959abf4d35e06c6e8d4e41287a9a
bq2cd <231322472+bq2cd@users.noreply.github.com>
2025-09-20 12:34:58 +0300

    handlers for reading metrics: '/' and '/value': implementation
    Implementation for 'readHandler' and 'valueHandler' to conform with
    the tests defined in the previous commit.

---
+++ internal/handler/value.go
@@ -0,0 +1,62 @@
+package handler
+
+import (
+	"bytes"
+	"errors"
+	"net/http"
+	"strconv"
+
+	"github.com/bq2cd/yp-go-metrics/internal/handler/urlpath"
+	"github.com/bq2cd/yp-go-metrics/internal/model"
+	"github.com/bq2cd/yp-go-metrics/internal/repository"
+	"github.com/bq2cd/yp-go-metrics/internal/service"
+)
+
+type valueHandler struct {
+	metrics service.Metrics
+}
+
+// ServeHTTP implements http.Handler for /value endpoint
+func (h *valueHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	metricOp := urlpath.NewOperationFromURLPath(r.URL.Path)
+	needle, err := metricOp.ToMetric()
+	switch err {
+	case urlpath.ErrMissingMetricID:
+		http.Error(w, "", http.StatusNotFound)
+		return
+	case urlpath.ErrMissingMetricValue:
+		// OK
+	default:
+		http.Error(w, "", http.StatusBadRequest)
+		return
+	}
+
+	metrics, err := h.metrics.Retrieve(needle.Key())
+	switch {
+	case errors.Is(err, repository.ErrMetricNotFound):
+		http.Error(w, "", http.StatusNotFound)
+		return
+	case err != nil:
+		http.Error(w, "", http.StatusInternalServerError)
+		return
+	}
+	if len(metrics) != 1 {
+		http.Error(w, "", http.StatusInternalServerError)
+		return
+	}
+
+	metric := metrics[0]
+
+	w.Header().Set("content-type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+
+	out := new(bytes.Buffer)
+	switch metric.Type {
+	case model.MetricTypeCounter:
+		out.WriteString(strconv.FormatInt(*metric.Delta, 10))
+	default:
+		out.WriteString(strconv.FormatFloat(*metric.Value, 'g', 10, 64))
+	}
+
+	w.Write(out.Bytes())
+}
