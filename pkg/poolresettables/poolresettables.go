1450d135ddd4e8ef33b32b30ce40a539c1575ab4
bq2cd <231322472+bq2cd@users.noreply.github.com>
2026-05-07 19:38:43 +0200

    feat(pool reset): a pool of "resettable" objects that can be reused
    Any object that implements `interface{ Reset() }` with pointer receiver
    (technically, value receivers also work due to Go generics limitations)
    can be stored in `poolresettables.Pool` container and retrieved later
    for a reuse.
    `bytes.Buffer` provides an example of such an object.
    
    The pool implementation ensures that an object being put back into the
    pool is "reset" to a "clean" state by calling the object's `Reset()`
    method. The call to `Reset()` is protected by the pool's mutex.
    
    Technically, nothing prevents the caller from using the object after
    calling `Put()` (and thus returning it to the pool) - because the pool
    stores a pointer to the object and returns this pointer to callers.
    So, when the caller is done with the object, it can both `Put()` it back
    to the pool (which will cause `Reset()` call for the object) and continue
    using the pointer (potentially modifying it).
    There does not seem to be an easy way to prevent such behavior
    for callers; `sync.Pool` from the standard library seems to have
    the same limitation.
    In theory, we can somewhat mitigate this by calling `Reset()` when a
    caller requests an object from the pool (in `Get()` method); this will
    incur some performance penalty due to `Reset()` logic being executed
    twice (on `Put()` and on `Get()`).

---
+++ pkg/poolresettables/poolresettables.go
@@ -0,0 +1,105 @@
+// Package poolresettables provides a container (pool) for object implementing `interface { Reset() }`.
+// Such objects can be put into the pool and have their `Reset()` method called automatically by the pool.
+// When an object is taken from the pool, it should be in a ready-to-use state (on the condition that
+// the object's `Reset()` method is implemented properly).
+package poolresettables
+
+import (
+	"sync"
+
+	"github.com/bq2cd/yp-go-metrics/pkg/option"
+)
+
+const (
+	defaultInitialPoolCapacity = 16
+)
+
+// Resettable describe an object that can be "reset" to a clean state and reused.
+// The object must have `Reset()` method implemented for a pointer receiver.
+type Resettable[T any] interface {
+	Reset()
+	*T
+}
+
+// Pool is a container for storing and retrieving [Resettable] objects in a concurrently-safe manner.
+// When stored in the [Pool], each object is "reset" to a clean state by calling its `Reset()` method.
+// The object should be designed to be ready for use after zero value allocation (with `new()`);
+// `bytes.Buffer` constitutes an example of such object.
+// If object requires any additional initialization on top of zero value allocation, it is
+// responsibility of the caller to either finish the object's initialization after zero value is
+// received or pre-populate pool with properly initialized objects.
+type Pool[T any, R Resettable[T]] struct {
+	mu      sync.RWMutex
+	objects []*T
+}
+
+type poolConfig struct {
+	InitialPoolCapacity uint
+}
+
+// New creates an instance of a [Pool].
+// Initial pool capacity can be configured by using [WithInitialCapacity] option.
+func New[T any, R Resettable[T]](opts ...option.Option[poolConfig]) *Pool[T, R] {
+	cfg := &poolConfig{
+		InitialPoolCapacity: defaultInitialPoolCapacity,
+	}
+
+	option.Apply(cfg, opts...)
+
+	return &Pool[T, R]{
+		objects: make([]*T, 0, cfg.InitialPoolCapacity),
+	}
+}
+
+// WithInitialCapacity configures [Pool] initial capacity.
+func WithInitialCapacity(capacity uint) option.Option[poolConfig] {
+	return func(c *poolConfig) {
+		c.InitialPoolCapacity = capacity
+	}
+}
+
+// Put stores provided [Resettable] object into the pool (`nil` objects are ignored).
+// The object's `Reset()` method is guaranteed to be called in a concurrently-safe manner
+// (that is, protected by [Pool]'s mutex) before the object is stored in the pool.
+// The caller **must not** use the object directly after it has been [Put] into the pool.
+func (p *Pool[T, R]) Put(obj R) {
+	if obj == nil {
+		return
+	}
+
+	p.mu.Lock()
+	defer p.mu.Unlock()
+
+	obj.Reset()
+
+	p.objects = append(p.objects, obj)
+}
+
+// Get retrieves a previously "reset" object from the pool if the pool is not empty.
+// Otherwise, it allocates a zero value (with `new()`) and returns a pointer to it.
+// It is a responsibility of the caller to finish object's initialization if zero value is received
+// (if such action is required).
+// The caller is also responsible for returning the object to the pool after the work is finished,
+// or discarding the object if it is no longer necessary.
+func (p *Pool[T, R]) Get() *T {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+
+	last := len(p.objects) - 1
+	if last < 0 {
+		return new(T) // allocate zero value
+	}
+
+	obj := p.objects[last]
+	p.objects = p.objects[:last]
+
+	return obj
+}
+
+// Size returns number of idle (obtainable with [Pool.Get]) objects in the pool.
+func (p *Pool[T, R]) Size() int {
+	p.mu.RLock()
+	defer p.mu.RUnlock()
+
+	return len(p.objects)
+}
